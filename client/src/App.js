3038d2e0d3a7e6c80f9f4e2726d74d2d2fa8f167
r3432 <duonglinh181200@gmail.com>
2024-09-19 16:37:12 +0700

    up

--- client/src/App.js
+++ client/src/App.js
@@ -26,20 +26,10 @@ function App() {
   return (
     <div className="bg-primary overflow-hidden">
       <Routes>
-        <Route path={path.HOME} element={<Home />}>
-          <Route path='*' element={<Homepage />} />
-          <Route path={path.LOGIN} element={<Login />} />
-          <Route path={path.CHO_THUE_CAN_HO} element={<Rental />} />
-          <Route path={path.CHO_THUE_MAT_BANG} element={<Rental />} />
-          <Route path={path.CHO_THUE_PHONG_TRO} element={<Rental />} />
-          <Route path={path.NHA_CHO_THUE} element={<Rental />} />
-          <Route path={path.SEARCH} element={<SearchDetail />} />
-          <Route path={path.DETAL_POST__TITLE__POSTID} element={<DetailPost />} />
-          <Route path={'chi-tiet/*'} element={<DetailPost />} />
-        </Route>
+    
         <Route path={path.SYSTEM} element={<System />} >
           <Route path={path.CREATE_POST} element={<CreatePost />} />
-          <Route path={path.MANAGE_POST} element={<ManagePost />} />
+        
         </Route>
 
       </Routes>
